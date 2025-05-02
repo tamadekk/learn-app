fb5dc93a58f61baaaf9dd78e30a4cbc1fa208844
tamadekk <186jnd@gmail.com>
2025-05-02 20:21:05 +0200

    feat(navbar): show actual user name and email

--- src/components/Header/ProfileCard/ProfileCard.tsx
+++ src/components/Header/ProfileCard/ProfileCard.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import Image from 'next/image';
 import { mockedAvatar } from '@/assets';
+import { useAuth } from '@/context/AuthContext';
 
 const ProfileCard = () => {
+	const { user } = useAuth();
 	return (
 		<div className='flex flex-row gap-4'>
 			<Image
-				src={mockedAvatar.src}
+				src={user?.photoURL || mockedAvatar.src}
 				alt='Profile Picture'
 				width={50}
 				height={50}
 			/>
-
 			<div>
-				<p className='font-bold'>John_12</p>
-				<p className='text-neutral-500 font-normal'>John_12@gmail.com</p>
+				<p className='font-bold'>{user?.displayName}</p>
+				<p className='text-neutral-500 font-normal'>{user?.email}</p>
 			</div>
 		</div>
 	);
